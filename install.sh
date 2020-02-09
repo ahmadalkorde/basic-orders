f2ff891d1963eb8d672dc61d27bcda9a9bf69db0
HACK_SYRIN <48409814+ahmadalkorde@users.noreply.github.com>
2020-02-09 09:11:35 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,159 @@
+#!/bin/bash
+echo $red 
+ echo ""####################### ""
+echo " This Script is written science DIY channel. "
+echo"##############################"
+
+echo "007"
+echo "007"
+echo "007"
+echo "."
+
+echo  "#############################"
+echo   "  SUBSCRIBE    scince BY"
+ echo   "#$ABNALKRDE#$         "                
+echo "     
+   "#############################
+
+echo "007"
+echo "007"
+echo "007"
+
+echo "########################"
+
+apt update -y                      #update in progress
+apt install python -y           #installation......
+apt install python2 -y
+apt install ruby -y 
+apt install git -y
+
+echo  "###########################"
+echo   "  SUBSCRIBE  science DIY "
+ echo   "   AK "                                                                    
+echo   "####################################"
+
+apt install php -y 
+apt install perl -y
+apt install nmap -y
+
+echo  "#####################################"
+echo   "  SUBSCRIBE    Science DIY " 
+ echo   "   AK"                                                                    
+echo   "####################################"
+
+apt install bash -y
+apt install clang -y
+apt install jq -y
+apt install macchanger -y
+
+echo  "#####################################"
+echo   "  SUBSCRIBE    science DIY "
+ echo   "   AK"                                                                    
+echo   "####################################"
+
+apt install nano -y
+apt install curl -y
+apt install tar -y
+apt install zip -y
+
+echo  "#####################################"
+echo   "  SUBSCRIBE    scince DIY "
+    echo   "  AK"                                                              
+echo   "####################################"
+
+apt install unzip -y
+apt install tor -y 
+apt install sudo -y
+apt install wget -y
+
+echo  "#####################################"
+echo   "  SUBSCRIBE    science DIY"
+ echo   "  AK "                                                                    
+echo   "####################################"
+
+apt install wcalc -y 
+apt install openssl
+apt install bmon -y
+
+echo  "#####################################"
+echo   "  SUBSCRIBE    science DIY "
+ echo   "   AK"                                                                    
+echo   "####################################"
+
+echo " If You Connected To Internet Then  "
+echo " YOUR PACKAGES IS INSTALLED SUCCESSFULLY "
+
+pkg update -y
+pkg upgrade -y
+pkg install python -y
+pkg install python2 -y
+pkg install python2-dev -y
+pkg install python3 -y
+pkg install java -y
+pkg install fish -y
+pkg install ruby -y
+pkg install help -y
+pkg install git -y
+pkg install host -y
+pkg install php -y
+pkg install perl -y
+pkg install nmap -y
+pkg install bash -y
+pkg install clang -y
+pkg install nano -y
+pkg install w3m -y
+pkg install havij -y
+pkg install hydra -y
+pkg install figlet -y
+pkg install cowsay -y
+pkg install curl -y
+pkg install tar -y
+pkg install zip -y
+pkg install unzip -y
+pkg install tor -y
+pkg install google -y
+pkg install sudo -y
+pkg install wget -y
+pkg install wireshark -y
+pkg install wgetrc -y
+pkg install wcalc -y
+pkg install bmon -y
+pkg install vpn -y
+pkg install unrar -y
+pkg install toilet -y
+pkg install proot -y
+pkg install net-tools -y
+pkg install golang -y
+ pkg install chroot -y
+ termux-chroot -y
+pkg install macchanger -y
+pkg install openssl -y
+pkg install cmatrix -y
+pkg install openssh -y
+pkg install wireshark -y
+termux-setup-storage -y
+apt install golang -y
+pkg install macchanger -y
+apt install openssh 
+apt install openssl-tool 
+apt install dropbear
+
+apt install git
+
+apt install sl
+sl 
+gem install appium_lib -v 10.3.0
+pkg install unstable-repo
+pkg install metasploit
+pkg install root-repo
+pkg install apk
+pkg install virustotal-cli
+to use a linux on termux (commands only) 
+apt install util-linux
+linux64 
+apt-get update
+apt-get install tor
+pkg install mtools
+
+pip install pip
+pip install --upgrade pip
